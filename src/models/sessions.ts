8a12c572233940c180636302a21784ee9f167c64
Rafael Mendes <mendesrafa.c@gmail.com>
2020-11-23 11:29:01 -0300

    adding feature that removes all tokens related to a user previous logged in

--- src/models/sessions.ts
+++ src/models/sessions.ts
@@ -1,11 +1,14 @@
 import _ from 'lodash'
 import { readDB, updateDB, SESSIONS_DATA_FILE } from '../utils/utils'
+import { findUserByID } from './user'
 
 type Session = { token: string, id: number }
 
 const sessions: Array<Session> = readDB(SESSIONS_DATA_FILE)
 
 const loadSession = (session: Session) => {
+  if (findUserByID(session)) _.remove(sessions, ['id', session.id])
+
   sessions.push(session)
   updateDB(SESSIONS_DATA_FILE, sessions)
 }
